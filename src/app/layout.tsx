7a278421d13933b673a9cd36023359640aa059b7
magician333 <magician33333@163.com>
2024-05-21 16:19:32 +0800

    add speed insights and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <SpeedInsights />
-          <Analytics />
         </ThemeProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
 
